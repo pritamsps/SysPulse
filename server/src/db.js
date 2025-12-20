7e0f03023f405e1405173e08e45b5395495c4ef3
Pritam Sanjay S <pritam.montfort@gmail.com>
2025-12-20 11:40:04 +0530

    feat: updated src/db.js

--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,28 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+
+const getPoolConfig = () => {
+    if (process.env.DATABASE_URL) {
+        console.log("🔌 Connecting to Cloud Database...");
+        return {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        };
+    }
+
+ 
+    console.log("🔌 Connecting to Local Database...");
+    return {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+    };
+};
+
+const pool = new Pool(getPoolConfig());
+
 module.exports = pool;
